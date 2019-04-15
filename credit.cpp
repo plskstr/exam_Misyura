d0ea0191cde852d26e08a486c266a965518e7fa5
anonymous <user@step.itstep.rv.ua>
2019-04-15 20:00:41 +0300

    Algorithm+

--- credit.cpp
+++ credit.cpp
@@ -5,6 +5,7 @@ int SumOdd(int* arr);
 int SumEven(int* arr);
 bool IsCorrect(const long long& number);
 void IsValid(int sum);
+void AlgorithmLuhn(long long& number);2
 
 int main()
 {
@@ -55,4 +56,15 @@ bool IsCorrect(const long long& number)
 void IsValid(int sum)
 {
 	cout << "Number of your card is " << (sum % 10 == 0 ? "" : "in") << "valid.\n";
+}
+
+void AlgorithmLuhn(long long& number)
+{
+	int* digits = FormArr(number);
+
+	int sum = 0;
+	sum += SumEven(digits);
+	sum += SumOdd(digits);
+
+	IsValid(sum);
 }
